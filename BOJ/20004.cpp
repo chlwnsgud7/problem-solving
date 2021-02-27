a3ee656e76e87ca868b99297bcc0d1ba71ed8dbc
Envil_Saintan <chlwnsgud7@gmail.com>
2021-02-27 13:43:23 +0900

    BOJ 20004 AC

---
+++ BOJ/20004.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+
+inline bool valid(int x, int n) {
+    return 0 <= x && x < n;
+}
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+template <typename T>
+inline T pop(queue<T>& q) {
+    T front = q.front();
+    q.pop();
+    return front;
+}
+
+template <typename T>
+inline T gcd(T a, T b) {
+    for (; b; a %= b, swap(a, b));
+    return a; 
+}
+
+template <typename T>
+tuple<T, T, T> xgcd(T a, T b) {
+    if (b == 0) return {1, 0, a};
+    T x, y, g;
+    tie(x, y, g) = xgcd(b, a%b);
+    return {y, x-(a/b)*y, g};
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int a; cin >> a;
+    for (int i = 1; i <= a; i++) {
+        if (31 % (i+1) == 1) cout << i << "\n";
+    }
+}

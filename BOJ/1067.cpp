07930a2acabf4b227f8251273f99855ce6a22f76
Envil_Saintan <chlwnsgud7@gmail.com>
2021-03-04 04:24:26 +0900

    FFT 구현 변경

--- BOJ/1067.cpp
+++ BOJ/1067.cpp
@@ -41,69 +41,50 @@ tuple<T, T, T> xgcd(T a, T b) {
     return {y, x-(a/b)*y, g};
 }
 
-/* FFT Demo */
-/* 출처 : https://justicehui.github.io/hard-algorithm/2019/09/04/FFT/ */
-
-typedef complex<double> cpx;
-typedef vector<cpx> vec;
-
-const double pi = acos(-1);
-
-/*
-input : f => Coefficient, w => principal n-th root of unity
-output : f => f(x_0), f(x_1), f(x_2), ... , f(x_n-1)
-T(N) = 2T(N/2) + O(N)
-*/
-void FFT(vec &f, cpx w){
-	int n = f.size();
-	if(n == 1) return; //base case
-	vec even(n >> 1), odd(n >> 1);
-	for(int i=0; i<n; i++){
-		if(i & 1) odd[i >> 1] = f[i];
-		else even[i >> 1] = f[i];
-	}
-	FFT(even, w*w); FFT(odd, w*w);
-	cpx wp(1, 0);
-	for(int i=0; i<n/2; i++){
-		f[i] = even[i] + wp * odd[i];
-		f[i+n/2] = even[i] - wp * odd[i];
-		wp = wp * w;
-	}
-}
-
-/*
-input : a => A's Coefficient, b => B's Coefficient
-output : A * B
-*/
-vec mul(vec a, vec b){
-	int n = 1;
-
-  //a * b 결과의 길이보다 길거나 같은 2의 멱수 찾기
-	while(n <= a.size() || n <= b.size()) n <<= 1;
-	n <<= 1;
-	a.resize(n); b.resize(n); vec c(n);
-
-  //principal n-th root of unity
-	cpx w(cos(2*pi/n), sin(2*pi/n));
-
-  //a와 b의 dft구하기
-	FFT(a, w); FFT(b, w);
-
-  //f(x) * g(x) = h(x)
-	for(int i=0; i<n; i++) c[i] = a[i] * b[i];
-
-  //켤레 복소수로 dft -> idft
-	FFT(c, cpx(w.real(), -w.imag()));
-	for(int i=0; i<n; i++){
-		c[i] /= cpx(n, 0);
-		c[i] = cpx(round(c[i].real()), round(c[i].imag())); // 만약 정수 결과를 원한다면
-	}
-	return c;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
+    using cpx = complex<double>;
+    using vec = vector<cpx>;
+    const double pi = 3.14159265358979;
+
+    function<vec(vec, vec)> mul = [pi] (vec a, vec b) -> vec {
+        function<void(vec&, cpx)> FFT = [&FFT](vec& f, cpx w) -> void {
+            int n = f.size();
+            if (n == 1) return;
+
+            vec even(n>>1), odd(n>>1);
+            for (int i = 0; i < n; i++) {
+                if (i & 1) odd[i>>1] = f[i];
+                else even[i>>1] = f[i];
+            }
+            FFT(even, w*w); FFT(odd, w*w);
+            cpx wp(1, 0);
+            for (int i = 0; i < n/2; i++) {
+                f[i] = even[i] + wp*odd[i];
+                f[i+n/2] = even[i] - wp*odd[i];
+                wp = wp * w;
+            }
+        };
+
+        int n = 1;
+        while (n <= a.size() || n <= b.size()) n <<= 1;
+        n <<= 1;
+        a.resize(n); b.resize(n); vec c(n);
+
+        cpx w(cos(2*pi/n), sin(2*pi/n));
+        FFT(a, w); FFT(b, w);
+        for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
+
+        FFT(c, cpx(w.real(), -w.imag()));
+        for (int i = 0; i < n; i++) {
+            c[i] /= cpx(n, 0);
+            c[i] = cpx(round(c[i].real()), round(c[i].imag())); /* 정수 결과 필요시 */
+        }
+
+        return c;
+    };
 
     int n; cin >> n;
     vi x(n); for (int i = 0; i < n; i++) cin >> x[i];
@@ -117,3 +98,5 @@ int main() {
     for (int i = n; i <= 2*n; i++) rst = max(rst, c[i].real());
     cout << fixed << setprecision(0) << rst;
 }
+
+// 풀이 출처 : https://justicehui.github.io/hard-algorithm/2019/09/04/FFT/
